50b45a1ed040db49f8629cf8c7b6eb0b70577bb5
vnget4u@gmail.com <vnget4u@gmail.com@08098daa-7d17-0e78-07bb-989c6761a6da>
2013-10-06 16:07:58 +0000

    c

---
+++ group.php
@@ -0,0 +1,8 @@
+var gsmin = [];
+var gsmod = [];
+var gmb = [];
+var gsb = [];
+var gvip1 = ["dark9k","tlandn","Destiny_Boy","Amanat","rada","Nguy&#7877;n Anh Duy","bjemtj","r.kan","ctxn52","Wolverine","poisongas","NTT_0309","Kanechita","dang quoc dat","leduong_98","Nguy&#7877;n Anh Duy","mrtbtrung","randomgacc","LM.10","Oxygen","dai_za911","qojqva","khoi_cunmem123","autumn","lovelyboy123","meossd","lmcuong1908","keangoo","darknight_2909","hackedpro","prettyb0y","vtc172889","minhtuan1411","zendocoro","mrde12","dragon_pig_95","ngodangtan","trinhoanh","gio193","madao","ngodangtan","thanhtu150","prodioto","vannhut07vn"];
+var gdon = [];
+var gspam = [];
+var gbad = [];
