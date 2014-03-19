bb2ed7ad19d69b9f6fb61ed039df4c080d31b317
jordicurto@gmail.com <jordicurto@gmail.com@08098daa-7d17-0e78-07bb-989c6761a6da>
2014-03-19 00:35:15 +0000

    Comunicacio entre servei activity i broadcastreceiver

--- autochecker/src/com/autochecker/service/AutoCheckerService.java
+++ autochecker/src/com/autochecker/service/AutoCheckerService.java
@@ -9,31 +9,71 @@ import android.content.Intent;
 import android.database.SQLException;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 import com.autochecker.data.AutoCheckerDataSource;
 import com.autochecker.data.model.WatchedLocation;
 
 public class AutoCheckerService extends Service {
+	
+	public static final String LOCATION_ALERT = "ALERT_LOCATION";
+	public static final String PROX_ALERT_INTENT = "ACTION_PROXIMITY_ALERT";
+	
+	public static final int MSG_REGISTER_CLIENT = 1;
+	public static final int MSG_UNREGISTER_CLIENT = 2;
+	
+	public static final int MSG_PROX_ALERT_DONE = 3;
 
 	private final String TAG = getClass().getSimpleName();
 
-	private LocationManager locationManager = null;
-	private AutoCheckerDataSource dataSource = null;
+	private LocationManager locationManager;
+	private AutoCheckerDataSource dataSource;
 
 	private static final long LOCATION_INTERVAL = -1;
+	
+	private class IncomingHandler extends Handler {
 
-	public static final String LOCATION_ALERT = "ALERT_LOCATION";
-	public static final String PROX_ALERT_INTENT = "ACTION_PROXIMITY_ALERT";
+		@Override
+		public void handleMessage(Message msg) {
+			
+            switch (msg.what) {
+            case MSG_REGISTER_CLIENT:
+            	replyMessenger = msg.replyTo;
+            	break;
+            case MSG_UNREGISTER_CLIENT:
+            	replyMessenger = null;
+            	break;
+            case MSG_PROX_ALERT_DONE:
+            	if (replyMessenger != null) {
+            		try {
+						replyMessenger.send(msg);
+					} catch (RemoteException e) {
+						Log.e(TAG, "Can't send proximity alert done message to service", e);
+					}
+            	}
+            default:
+            	super.handleMessage(msg);
+            }
+		}
+	}
+	
+	private final Messenger messenger = new Messenger(new IncomingHandler());
+	
+	private Messenger replyMessenger;
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		return messenger.getBinder();
 	}
 
 	@Override
 	public void onCreate() {
+		
 		Log.i(TAG, "onCreate");
 
 		initialize();
@@ -45,11 +85,19 @@ public class AutoCheckerService extends Service {
 			addProximityAlert(location);
 		}
 	}
+	
+	@Override
+	public void onDestroy() {
+		
+		dataSource.close();
+		
+		super.onDestroy();
+	}
 
 	private void addProximityAlert(WatchedLocation location) {
 
 		Log.d(TAG, "Adding proximity alert " + location.toString());
-
+		
 		locationManager.addProximityAlert(location.getLatitude(),
 				location.getLongitude(), location.getAccuracy(),
 				LOCATION_INTERVAL, getPendingIntent(location));
